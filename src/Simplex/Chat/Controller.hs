fcbb6e762a34ff270010bbf4eb375a64e018cefa
luka dodelia <luka@lnh.ge>
2026-07-10 20:11:09 +0400

    core: prevent full remote queue blocking mobile

--- src/Simplex/Chat/Controller.hs
+++ src/Simplex/Chat/Controller.hs
@@ -1608,6 +1608,7 @@ data RemoteCtrlSession
         rcsSession :: RCCtrlSession,
         http2Server :: Async (),
         remoteOutputQ :: TBQueue (Either ChatError ChatEvent),
+        remoteOutputQFull :: TMVar (),
         ctrlAppInfo :: CtrlAppInfo
       }
 
@@ -1708,9 +1709,10 @@ toView_ ev = do
     Nothing -> pure ev
   atomically $
     readTVar session >>= \case
-      Just (_, RCSessionConnected {remoteOutputQ})
+      Just (_, RCSessionConnected {remoteOutputQ, remoteOutputQFull})
         | either (const True) allowRemoteEvent event -> do
-            writeTBQueue remoteOutputQ event
+            remoteQFull <- isFullTBQueue remoteOutputQ
+            if remoteQFull then tryPutTMVar remoteOutputQFull () >> pure () else writeTBQueue remoteOutputQ event
             writeTBQueue localQ (Nothing, event)
       -- TODO potentially, it should hold some events while connecting
       _ -> writeTBQueue localQ (Nothing, event)
